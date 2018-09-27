8d47300b9d7a369f5ea5b4e095b6080fc9929696
wpbdry <wpbdry@gmail.com>
2018-09-27 18:28:26 +0200

    db structure changed

---
+++ add-alejs-example-tasks.sql
@@ -0,0 +1,180 @@
+
+INSERT INTO stepify.tasks (title, details, deadline_type, deadline, location_text, location_url, wiki, slack, calendar, other,
+    s_e, i_d, p_m, undecided, mandatory, outdoor, formal, swimming, pets, food)
+
+    /* EDIT THE THE FOLLOWING LINES FOR EACH TASK. DO NOT CHANGE THE ORDER OF THE LINES */
+VALUES (
+    'Meet outside of the Factory to go on a trip to a secret place',                        -- Replace text between quotes with task name
+    'We will all meet just outside of the Factory main entrance. Nextgen C<>DE t-shirts and wristbands will be handed out. We will board 3 buses that will take us to a secret destination were we will spend the next couple of days. We will be back by Thursday at 6pm',                 -- Replace text between quotes with task description
+
+    2,                                  -- 0 if there is no deadline, 1 if ASAP, 2 if deadline is finite time.
+    TIMESTAMP '2018-08-27 08:00:00',    -- 27.08.18, 8:00am Replace between quotes with actual date and time (in this format)
+    'Outside of Factory Görlitzer Park',                 -- or name of website, if "location" is online
+    'https://goo.gl/maps/oo52ytHLfnq',                  -- Either google maps url, or url to website
+
+    /* Do not use quotes in the following lines */
+    FALSE,  -- TRUE if this is a wiki activity, else FALSE
+    FALSE, -- TRUE if this is a slack activity, else FALSE
+    FALSE, -- TRUE if this is a calendar activity, else FALSE
+    TRUE, -- TRUE if this activity, is related to neither wiki, slack, nor calendar, else FALSE
+
+    TRUE,  -- TRUE if applicable to students enrolled in SE, else FALSE
+    TRUE,  -- TRUE if applicable to students enrolled in ID, else FALSE
+    TRUE,  -- TRUE if applicable to students enrolled in PM, else FALSE
+    TRUE,  -- TRUE if applicable to students who have not yet decided, else FALSE
+
+    TRUE, -- TRUE if task is mandatory, FALSE if task is optional
+
+    TRUE, -- TRUE if this is an outdoor activity
+    FALSE,  -- TRUE if this is a formal event/activity
+    TRUE, -- TRUE if it is recommended to bring a swimming suit
+    TRUE, -- TRUE if this is a pet friendly activity / event
+
+    2); -- 0 if event offers no vegan or vegetarian options. 1 for vegetarian. 2 for vegan.
+
+
+
+
+INSERT INTO stepify.tasks (title, details, deadline_type, deadline, location_text, location_url, wiki, slack, calendar, other,
+    s_e, i_d, p_m, undecided, mandatory, outdoor, formal, swimming, pets, food)
+
+    /* EDIT THE THE FOLLOWING LINES FOR EACH TASK. DO NOT CHANGE THE ORDER OF THE LINES */
+VALUES (
+    'Meet at the main hall for breakfast',                        -- Replace text between quotes with task name
+    'We will all meet at the main hall to have breakfast and then Jonathan will be sharing some thoughts about what it means to study at C<>DE.',                 -- Replace text between quotes with task description
+
+    2,                                  -- 0 if there is no deadline, 1 if ASAP, 2 if deadline is finite time.
+    TIMESTAMP '2018-08-18 09:00:00',    -- 28.08.18, 9:00am Replace between quotes with actual date and time (in this format)
+    'Resort Lobby',                 -- or name of website, if "location" is online
+    'https://goo.gl/maps/crvq9okpiCv',                  -- Either google maps url, or url to website
+
+    /* Do not use quotes in the following lines */
+    FALSE,  -- TRUE if this is a wiki activity, else FALSE
+    FALSE, -- TRUE if this is a slack activity, else FALSE
+    FALSE, -- TRUE if this is a calendar activity, else FALSE
+    TRUE, -- TRUE if this activity, is related to neither wiki, slack, nor calendar, else FALSE
+
+    TRUE,  -- TRUE if applicable to students enrolled in SE, else FALSE
+    TRUE,  -- TRUE if applicable to students enrolled in ID, else FALSE
+    TRUE,  -- TRUE if applicable to students enrolled in PM, else FALSE
+    TRUE,  -- TRUE if applicable to students who have not yet decided, else FALSE
+
+    FALSE, -- TRUE if task is mandatory, FALSE if task is optional
+
+    FALSE, -- TRUE if this is an outdoor activity
+    FALSE,  -- TRUE if this is a formal event/activity
+    FALSE, -- TRUE if it is recommended to bring a swimming suit
+    TRUE, -- TRUE if this is a pet friendly activity / event
+
+    2); -- 0 if event offers no vegan or vegetarian options. 1 for vegetarian. 2 for vegan.
+
+
+
+
+INSERT INTO stepify.tasks (title, details, deadline_type, deadline, location_text, location_url, wiki, slack, calendar, other,
+    s_e, i_d, p_m, undecided, mandatory, outdoor, formal, swimming, pets, food)
+
+    /* EDIT THE THE FOLLOWING LINES FOR EACH TASK. DO NOT CHANGE THE ORDER OF THE LINES */
+VALUES (
+    'Pay for the student ID',                        -- Replace text between quotes with task name
+    'This student ID will give you access to all the public transportation in Berlin (Potsdam included). Of course it will also give you access to all student discounts'||E'\n\n'||'Once you have paid for it, please pick it up with Ina.'||E'\n\n'||'Prices semester ticket:'||E'\n'||'6 months is 193.80 Euro.'||E'\n'||'for enrollment on August 1: 161.50 Euro'||E'\n'||'for enrollment on September 1: 129.20 Euro'||E'\n\n'||'Bank transfer:'||E'\n'||'CODE Education GmbH, IBAN DE43 1005 0000 0190 5692 63, BIC BELADEBEXXX'||E'\n\n'||'PayPal transfer:'||E'\n'||'billing@code.berlin (please identify the payment as “friends or family” to avoid fees)',                 -- Replace text between quotes with task description
+
+    1,                                  -- 0 if there is no deadline, 1 if ASAP, 2 if deadline is finite time.
+    NULL,    -- Replace between quotes with actual date and time (in this format)
+    'PayPal or bank transfer',                 -- or name of website, if "location" is online
+    'www.paypal.com',                  -- Either google maps url, or url to website
+
+    /* Do not use quotes in the following lines */
+    FALSE,  -- TRUE if this is a wiki activity, else FALSE
+    FALSE, -- TRUE if this is a slack activity, else FALSE
+    FALSE, -- TRUE if this is a calendar activity, else FALSE
+    TRUE, -- TRUE if this activity, is related to neither wiki, slack, nor calendar, else FALSE
+
+    TRUE,  -- TRUE if applicable to students enrolled in SE, else FALSE
+    TRUE,  -- TRUE if applicable to students enrolled in ID, else FALSE
+    TRUE,  -- TRUE if applicable to students enrolled in PM, else FALSE
+    TRUE,  -- TRUE if applicable to students who have not yet decided, else FALSE
+
+    TRUE, -- TRUE if task is mandatory, FALSE if task is optional
+
+    FALSE, -- TRUE if this is an outdoor activity
+    FALSE,  -- TRUE if this is a formal event/activity
+    FALSE, -- TRUE if it is recommended to bring a swimming suit
+    FALSE, -- TRUE if this is a pet friendly activity / event
+
+    0); -- 0 if event offers no vegan or vegetarian options. 1 for vegetarian. 2 for vegan.
+
+
+
+
+INSERT INTO stepify.tasks (title, details, deadline_type, deadline, location_text, location_url, wiki, slack, calendar, other,
+    s_e, i_d, p_m, undecided, mandatory, outdoor, formal, swimming, pets, food)
+
+    /* EDIT THE THE FOLLOWING LINES FOR EACH TASK. DO NOT CHANGE THE ORDER OF THE LINES */
+VALUES (
+    'Attend Factory on-boarding',                        -- Replace text between quotes with task name
+    'Factory staff will give us an intro about what Factory is all about and give us a tour. We will be having croisants and pretzels!',                 -- Replace text between quotes with task description
+
+    2,                                  -- 0 if there is no deadline, 1 if ASAP, 2 if deadline is finite time.
+    TIMESTAMP '2011-08-31 10:00:00',    -- 31.08.18, 10:00am Replace between quotes with actual date and time (in this format)
+    'Factory Görlitzer Park',                 -- or name of website, if "location" is online
+    'https://goo.gl/maps/WMjXshcAnCN2',                  -- Either google maps url, or url to website
+
+    /* Do not use quotes in the following lines */
+    FALSE,  -- TRUE if this is a wiki activity, else FALSE
+    FALSE, -- TRUE if this is a slack activity, else FALSE
+    FALSE, -- TRUE if this is a calendar activity, else FALSE
+    TRUE, -- TRUE if this activity, is related to neither wiki, slack, nor calendar, else FALSE
+
+    TRUE,  -- TRUE if applicable to students enrolled in SE, else FALSE
+    TRUE,  -- TRUE if applicable to students enrolled in ID, else FALSE
+    TRUE,  -- TRUE if applicable to students enrolled in PM, else FALSE
+    TRUE,  -- TRUE if applicable to students who have not yet decided, else FALSE
+
+    TRUE, -- TRUE if task is mandatory, FALSE if task is optional
+
+    FALSE, -- TRUE if this is an outdoor activity
+    FALSE,  -- TRUE if this is a formal event/activity
+    FALSE, -- TRUE if it is recommended to bring a swimming suit
+    FALSE, -- TRUE if this is a pet friendly activity / event
+
+    0); -- 0 if event offers no vegan or vegetarian options. 1 for vegetarian. 2 for vegan.
+
+
+
+
+INSERT INTO stepify.tasks (title, details, deadline_type, deadline, location_text, location_url, wiki, slack, calendar, other,
+    s_e, i_d, p_m, undecided, mandatory, outdoor, formal, swimming, pets, food)
+
+    /* EDIT THE THE FOLLOWING LINES FOR EACH TASK. DO NOT CHANGE THE ORDER OF THE LINES */
+VALUES (
+    'Make a password for your brand new email @code.berlin!',                        -- Replace text between quotes with task name
+    'You will be using this email for everything C<>DE related. It will also come in handy when you need to use it at any institution that requires you to be a student.',                 -- Replace text between quotes with task description
+
+    1,                                  -- 0 if there is no deadline, 1 if ASAP, 2 if deadline is finite time.
+    NULL,    -- Replace between quotes with actual date and time (YYYY-MM-DD hh:mm:ss)
+    'code.berlin',                 -- or name of website, if "location" is online
+    'www.google.com/code.berlin/',                  -- Either google maps url, or url to website
+
+    /* Do not use quotes in the following lines */
+    FALSE,  -- TRUE if this is a wiki activity, else FALSE
+    FALSE, -- TRUE if this is a slack activity, else FALSE
+    FALSE, -- TRUE if this is a calendar activity, else FALSE
+    TRUE, -- TRUE if this activity, is related to neither wiki, slack, nor calendar, else FALSE
+
+    TRUE,  -- TRUE if applicable to students enrolled in SE, else FALSE
+    TRUE,  -- TRUE if applicable to students enrolled in ID, else FALSE
+    TRUE,  -- TRUE if applicable to students enrolled in PM, else FALSE
+    TRUE,  -- TRUE if applicable to students who have not yet decided, else FALSE
+
+    TRUE, -- TRUE if task is mandatory, FALSE if task is optional
+
+    FALSE, -- TRUE if this is an outdoor activity
+    FALSE,  -- TRUE if this is a formal event/activity
+    FALSE, -- TRUE if it is recommended to bring a swimming suit
+    FALSE, -- TRUE if this is a pet friendly activity / event
+
+    0); -- 0 if event offers no vegan or vegetarian options. 1 for vegetarian. 2 for vegan.
+
+
+
--- create-example-tasks.sql
+++ create-example-tasks.sql
@@ -1,21 +1,38 @@
-INSERT INTO stepify.tasks (task_name, task_details, s_e, i_d, p_m, undecided)
-VALUES ('task1', 'se task example', TRUE, FALSE, FALSE, FALSE);
 
-INSERT INTO stepify.tasks (task_name, task_details, s_e, i_d, p_m, undecided)
-VALUES ('task2', 'id task example', FALSE, TRUE, FALSE, FALSE);
+INSERT INTO stepify.tasks (title, details, deadline_type, deadline, location_text, location_url, wiki, slack, calendar, other,
+    s_e, i_d, p_m, undecided, mandatory, outdoor, formal, swimming, pets, tech, food)
 
-INSERT INTO stepify.tasks (task_name, task_details, s_e, i_d, p_m, undecided)
-VALUES ('task3', 'pm task example', FALSE, FALSE, TRUE, FALSE);
+    /* EDIT THE THE FOLLOWING LINES FOR EACH TASK. DO NOT CHANGE THE ORDER OF THE LINES */
+VALUES (
+    'task name',                        -- Replace text between quotes with task name
+    'task description',                 -- Replace text between quotes with task description. Use <br> for line break and use it twice to start a new paragraph
 
-INSERT INTO stepify.tasks (task_name, task_details, s_e, i_d, p_m, undecided)
-VALUES ('task4', 'se and id task example', TRUE, TRUE, FALSE, FALSE);
+    2,                                  -- 0 if there is no deadline, 1 if ASAP, 2 if deadline is finite time.
+    TIMESTAMP '2018-05-16 00:00:00',    -- Replace between quotes with actual date and time (YYYY-MM-DD hh:mm:ss).
+                                        -- Delete entire line above and type NULL (without quotes) if it's 0 or 1 above
+    'name of location',                 -- or name of website, if "location" is online
+    'url of location',                  -- Either google maps url, or url to website
 
-INSERT INTO stepify.tasks (task_name, task_details, s_e, i_d, p_m, undecided)
-VALUES ('task5', 'task for everyone!', TRUE, TRUE, TRUE, TRUE);
+    /* Do not use quotes in the following lines */
+    FALSE,  -- TRUE if this is a wiki activity, else FALSE
+    FALSE, -- TRUE if this is a slack activity, else FALSE
+    FALSE, -- TRUE if this is a calendar activity, else FALSE
+    TRUE, -- TRUE if this activity, is related to neither wiki, slack, nor calendar, else FALSE
 
-INSERT INTO stepify.tasks (task_name, task_details, s_e, i_d, p_m, undecided)
-VALUES ('task6', 'task for undecided people!', FALSE, FALSE, FALSE, FALSE);
+    TRUE,  -- TRUE if applicable to students enrolled in SE, else FALSE
+    TRUE,  -- TRUE if applicable to students enrolled in ID, else FALSE
+    TRUE,  -- TRUE if applicable to students enrolled in PM, else FALSE
+    TRUE,  -- TRUE if applicable to students who have not yet decided, else FALSE
 
-INSERT INTO stepify.tasks (task_name, task_details, s_e, i_d, p_m, undecided)
-VALUES ('task7', 'task for PMs and undecided ppl!', FALSE, FALSE, TRUE, TRUE);
+    FALSE, -- TRUE if task is mandatory, FALSE if task is optional
+
+    FALSE, -- TRUE if this is an outdoor activity
+    FALSE,  -- TRUE if this is a formal event/activity
+    FALSE, -- TRUE if it is recommended to bring a swimming suit
+    FALSE, -- TRUE if this is a pet friendly activity / event
+    FALSE, -- TRUE if this is a techie activity or event
+
+    0); -- 0 if event offers no vegan or vegetarian options. 1 for vegetarian. 2 for vegan.
+
+
 
