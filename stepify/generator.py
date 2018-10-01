3e8aab708ff35c2119bcee50cc66122d7d8e9338
William Dry <wpbdry@gmail.com>
2018-10-01 21:31:23 +0200

    file checking function when creating session id working

--- stepify/generator.py
+++ stepify/generator.py
@@ -1,8 +1,22 @@
 
 import string
 import random
+import os.path
 
 
-def char_id(size):
+# Function that creates a random id, takes length as only param
+def generate_char_id(size):
     chars = string.ascii_uppercase + string.digits
     return ''.join(random.choice(chars) for _ in range(size))
+
+
+# Function to generate codes for cookies and pems - checks if file exists before it creates it
+def generate_session_id():
+    while True:
+        # Create random code
+        ident = generate_char_id(6)
+
+        # Check if a file with that name already exists
+        name = 'keys/' + ident + '_private_key.pem'
+        if not os.path.isfile(name):
+            return ident
