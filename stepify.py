aaeb48ea85532b6f5ef6d3602a40d4fa184ff5a1
William Dry <wpbdry@gmail.com>
2018-10-07 13:00:08 +0200

    working

--- stepify.py
+++ stepify.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 
 def find_user_from_uname(un):  # finds user by username and returns id
-    uid = dbconnect.query("SELECT id FROM stepify.users WHERE username = %;",
+    uid = dbconnect.query("SELECT id FROM stepify.users WHERE username = %s;",
                           (un,),
                           "one")
     if uid:
@@ -32,10 +32,10 @@ def show_main_page():
     # get list of tasks
     tasks = dbconnect.query_json(open("get-tasks.sql", "r").read(), (uid,))
     # get data for progress bar
-    total_tasks = dbconnect.query("SELECT COUNT(id) FROM stepify.users_tasks WHERE user_id = %;",
+    total_tasks = dbconnect.query("SELECT COUNT(id) FROM stepify.users_tasks WHERE user_id = %s;",
                                   (str(uid),),
                                   "one")[0]
-    done_tasks = dbconnect.query("SELECT COUNT(id) FROM stepify.users_tasks WHERE user_id = % AND completion = TRUE;",
+    done_tasks = dbconnect.query("SELECT COUNT(id) FROM stepify.users_tasks WHERE user_id = %s AND completion = TRUE;",
                                  (str(uid),),
                                  "one")[0]
 
@@ -55,7 +55,7 @@ def show_main_page():
 
 def check_login():  # Returns username if user is logged in and returns False if not
     uip = request.remote_addr  # user's current ip
-    usern = dbconnect.query("SELECT username FROM stepify.logins WHERE ip = %;",
+    usern = dbconnect.query("SELECT username FROM stepify.logins WHERE ip = %s;",
                             (uip,),
                             "one")
     if usern is None:
@@ -66,14 +66,14 @@ def check_login():  # Returns username if user is logged in and returns False if
 
 def log_user_in(un):  # record that user is logged in
     ip = request.remote_addr  # user's current ip
-    dbconnect.write("INSERT INTO stepify.logins (username, ip) VALUES (%, %);",
+    dbconnect.write("INSERT INTO stepify.logins (username, ip) VALUES (%s, %s);",
                     (un, ip,))
 
 
 def log_user_out(un):
     ip = request.remote_addr  # user's current ip
     # Check if user is logged in
-    logged_in_uname = dbconnect.query("SELECT username FROM stepify.logins WHERE ip = %;",
+    logged_in_uname = dbconnect.query("SELECT username FROM stepify.logins WHERE ip = %s;",
                                       (ip,),
                                       "one")
 
@@ -83,7 +83,7 @@ def log_user_out(un):
 
     # if logged in, log out
     if logged_in_uname == un:
-        dbconnect.write("DELETE FROM stepify.logins WHERE username = % AND ip = %;",
+        dbconnect.write("DELETE FROM stepify.logins WHERE username = %s AND ip = %s;",
                         (un, ip,))
 
     else:
@@ -96,7 +96,7 @@ def process_login(u):
         return un + ' is not yet registered. <a href="signup">Sign up now</a>'
     else:
         uid = find_user_from_uname(un)
-        pw = dbconnect.query("SELECT password FROM stepify.users WHERE id = %;",
+        pw = dbconnect.query("SELECT password FROM stepify.users WHERE id = %s;",
                              (str(uid),),
                              "one")[0]  # get correct password
         if pw == u['password']:  # if password is correct
@@ -113,7 +113,7 @@ def process_signup(u):
     if find_user_from_uname(un) is None:
         # Sign user up
         dbconnect.write(
-            "INSERT INTO stepify.users (username, password) VALUES (%, %);",
+            "INSERT INTO stepify.users (username, password) VALUES (%s, %s);",
             (un, u['password'],))
         # Log user in
         log_user_in(un)
@@ -126,15 +126,17 @@ def process_signup(u):
 def set_user_sp_and_tasks(study_program):
     un = check_login()
     # record user's study program
-    dbconnect.write("UPDATE stepify.users SET study_program = % WHERE username = %;",
+    dbconnect.write("UPDATE stepify.users SET study_program = %s WHERE username = %s;",
                     (study_program, un,))
 
     # set user's tasks based on study program
 
     # first get a list of tasks (as ids) relevant to this study program
-    task_ids = dbconnect.query("SELECT id FROM stepify.tasks WHERE % = TRUE;",
-                               (study_program,),
-                               "all")
+    # Psycopg2 automatically wraps things with %s in single quotes, so I am forced to directly insert a string here
+    # First I will check that the string is one of the 4 options we expect it to be
+    if study_program == 's_e' or study_program == 'i_d' or study_program == 'p_m' or study_program == 'undecided':
+        sql_string = 'SELECT id FROM stepify.tasks WHERE ' + study_program + ' = TRUE;'
+        task_ids = dbconnect.query(sql_string, (), "all")
 
     # then get user's id (in users table)
     user_id = find_user_from_uname(un)
@@ -142,7 +144,7 @@ def set_user_sp_and_tasks(study_program):
     # then link the tasks to the user in the users_tasks table
     for task_id in task_ids:
         dbconnect.write(
-            "INSERT INTO stepify.users_tasks (user_id, task_id, completion) VALUES (%, %, FALSE)",
+            "INSERT INTO stepify.users_tasks (user_id, task_id, completion) VALUES (%s, %s, FALSE)",
             (str(user_id), str(task_id[0]),))
 
 
@@ -206,7 +208,7 @@ def task_done_id():
         uid = str(find_user_from_uname(uname))
 
         dbconnect.write(
-            "UPDATE stepify.users_tasks SET completion = TRUE, completion_date = TIMESTAMP % WHERE user_id = % AND task_id = %;",
+            "UPDATE stepify.users_tasks SET completion = TRUE, completion_date = TIMESTAMP %s WHERE user_id = %s AND task_id = %s;",
             (task_data['compdate'], uid, task_data['taskid'],))
 
     # the code below is executed if the request method was GET
@@ -224,7 +226,7 @@ def task_undone():
         uid = str(find_user_from_uname(uname))
 
         dbconnect.write(
-            "UPDATE stepify.users_tasks SET completion = FALSE WHERE user_id = % AND task_id = %;",
+            "UPDATE stepify.users_tasks SET completion = FALSE WHERE user_id = %s AND task_id = %s;",
             (uid, task_id,))
 
     # the code below is executed if the request method was GET
