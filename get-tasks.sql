e52f8676f3c1726b47929a134900be2e9f56900f
William Dry <wpbdry@gmail.com>
2018-10-07 12:18:19 +0200

    getting errors

--- get-tasks.sql
+++ get-tasks.sql
@@ -2,7 +2,7 @@ SELECT *
     FROM stepify.tasks
     JOIN stepify.users_tasks
         ON stepify.users_tasks.task_id = stepify.tasks.id
-    WHERE stepify.users_tasks.user_id = %s
+    WHERE stepify.users_tasks.user_id = %
     ORDER BY
         deadline_type ASC,
         deadline ASC;
--- stepify.py
+++ stepify.py
@@ -30,12 +30,14 @@ def show_main_page():
     uid = str(find_user_from_uname(uname))
 
     # get list of tasks
-    tasks = dbconnect.query_json(open("get-tasks.sql", "r").read() % uid)
+    tasks = dbconnect.query_json(open("get-tasks.sql", "r").read(), (uid,))
     # get data for progress bar
     total_tasks = dbconnect.query("SELECT COUNT(id) FROM stepify.users_tasks WHERE user_id = %;",
-                                  (uid,),
+                                  (str(uid),),
                                   "one")[0]
-    done_tasks = dbconnect.query("SELECT COUNT(id) FROM stepify.users_tasks WHERE user_id = %;", (str(uid),),  "'" + str(uid) + "' AND completion = TRUE", "one")[0]
+    done_tasks = dbconnect.query("SELECT COUNT(id) FROM stepify.users_tasks WHERE user_id = % AND completion = TRUE;",
+                                 (str(uid),),
+                                 "one")[0]
 
     # load page and send task info to js
     # also send completed tasks and total tasks to that front end can calculate progress
@@ -53,8 +55,9 @@ def show_main_page():
 
 def check_login():  # Returns username if user is logged in and returns False if not
     uip = request.remote_addr  # user's current ip
-    usern = dbconnect.query("SELECT username FROM stepify.logins WHERE ip = '" + uip + "'",
-                     "one")
+    usern = dbconnect.query("SELECT username FROM stepify.logins WHERE ip = %;",
+                            (uip,),
+                            "one")
     if usern is None:
         return False
     else:
@@ -63,14 +66,16 @@ def check_login():  # Returns username if user is logged in and returns False if
 
 def log_user_in(un):  # record that user is logged in
     ip = request.remote_addr  # user's current ip
-    dbconnect.write("INSERT INTO stepify.logins (username, ip) VALUES ('" + un + "', '" + ip + "');")
+    dbconnect.write("INSERT INTO stepify.logins (username, ip) VALUES (%, %);",
+                    (un, ip,))
 
 
 def log_user_out(un):
     ip = request.remote_addr  # user's current ip
     # Check if user is logged in
-    logged_in_uname = dbconnect.query("SELECT username FROM stepify.logins WHERE ip = '" + ip + "';",
-                               "one")
+    logged_in_uname = dbconnect.query("SELECT username FROM stepify.logins WHERE ip = %;",
+                                      (ip,),
+                                      "one")
 
     # if someone is logged in, fix the uname
     if logged_in_uname:
@@ -78,7 +83,8 @@ def log_user_out(un):
 
     # if logged in, log out
     if logged_in_uname == un:
-        dbconnect.write("DELETE FROM stepify.logins WHERE username = '" + un + "' AND ip = '" + ip + "'")
+        dbconnect.write("DELETE FROM stepify.logins WHERE username = % AND ip = %;",
+                        (un, ip,))
 
     else:
         return redirect("/")
@@ -90,8 +96,9 @@ def process_login(u):
         return un + ' is not yet registered. <a href="signup">Sign up now</a>'
     else:
         uid = find_user_from_uname(un)
-        pw = dbconnect.query("SELECT password FROM stepify.users WHERE id = " + str(uid),
-                      "one")[0]  # get correct password
+        pw = dbconnect.query("SELECT password FROM stepify.users WHERE id = %;",
+                             (str(uid),),
+                             "one")[0]  # get correct password
         if pw == u['password']:  # if password is correct
             # Log user in
             log_user_in(un)
@@ -106,8 +113,8 @@ def process_signup(u):
     if find_user_from_uname(un) is None:
         # Sign user up
         dbconnect.write(
-            "INSERT INTO stepify.users (username, password) VALUES ('"
-            + un + "', '" + u['password'] + "');")
+            "INSERT INTO stepify.users (username, password) VALUES (%, %);",
+            (un, u['password'],))
         # Log user in
         log_user_in(un)
         # Move on to choose your study program
@@ -119,13 +126,15 @@ def process_signup(u):
 def set_user_sp_and_tasks(study_program):
     un = check_login()
     # record user's study program
-    dbconnect.write("UPDATE stepify.users SET study_program = '" + study_program + "' WHERE username = '" + un + "'")
+    dbconnect.write("UPDATE stepify.users SET study_program = % WHERE username = %;",
+                    (study_program, un,))
 
     # set user's tasks based on study program
 
     # first get a list of tasks (as ids) relevant to this study program
-    task_ids = dbconnect.query("SELECT id FROM stepify.tasks WHERE " + study_program + " = TRUE;",
-                        "all")
+    task_ids = dbconnect.query("SELECT id FROM stepify.tasks WHERE % = TRUE;",
+                               (study_program,),
+                               "all")
 
     # then get user's id (in users table)
     user_id = find_user_from_uname(un)
@@ -133,8 +142,8 @@ def set_user_sp_and_tasks(study_program):
     # then link the tasks to the user in the users_tasks table
     for task_id in task_ids:
         dbconnect.write(
-            "INSERT INTO stepify.users_tasks (user_id, task_id, completion) VALUES ('"
-            + str(user_id) + "', '" + str(task_id[0]) + "', FALSE);")
+            "INSERT INTO stepify.users_tasks (user_id, task_id, completion) VALUES (%, %, FALSE)",
+            (str(user_id), str(task_id[0]),))
 
 
 # ROUTES ###
@@ -197,9 +206,8 @@ def task_done_id():
         uid = str(find_user_from_uname(uname))
 
         dbconnect.write(
-            "UPDATE stepify.users_tasks SET completion = TRUE, completion_date = TIMESTAMP '"
-            + task_data['compdate'] + "' WHERE user_id = "
-            + uid + " AND task_id = '" + task_data['taskid'] + "';")
+            "UPDATE stepify.users_tasks SET completion = TRUE, completion_date = TIMESTAMP % WHERE user_id = % AND task_id = %;",
+            (task_data['compdate'], uid, task_data['taskid'],))
 
     # the code below is executed if the request method was GET
     return render_template('404.html', error=error)
@@ -216,8 +224,8 @@ def task_undone():
         uid = str(find_user_from_uname(uname))
 
         dbconnect.write(
-            "UPDATE stepify.users_tasks SET completion = FALSE WHERE user_id = "
-            + uid + " AND task_id = '" + task_id + "';")
+            "UPDATE stepify.users_tasks SET completion = FALSE WHERE user_id = % AND task_id = %;",
+            (uid, task_id,))
 
     # the code below is executed if the request method was GET
     return render_template('404.html', error=error)
