4fa8bcc89b0c33bc3ba304d64c0221e80e390724
William Dry <wpbdry@gmail.com>
2018-10-08 20:06:56 +0200

    This is what was running at the expo

---
+++ setup/vote-for-stepify.sql
@@ -0,0 +1,40 @@
+
+INSERT INTO stepify.tasks (title, details, deadline_type, deadline, location_text, location_url, wiki, slack, calendar, other,
+    s_e, i_d, p_m, undecided, mandatory, outdoor, formal, swimming, pets, tech, food, weather)
+
+    /* EDIT THE THE FOLLOWING LINES FOR EACH TASK. DO NOT CHANGE THE ORDER OF THE LINES */
+VALUES (
+    'Vote for Stepify!',                        -- Replace text between quotes with task name
+    'You have a chance to vote for the Stepify app in four different categories! Best booth design, best technology, best results and best attempt. Simply visit the ''Vote!'' link above, sign in with your CODE email address, and vote.',                 -- Replace text between quotes with task description. Use <br> for line break and use it twice to start a new paragraph
+    -- Other HTML markup is also possible, but single quotes must be properly escaped for postgresql
+
+    1,                                  -- 0 if ASAP, 1 if deadline is finite time, 2 if there is no deadline.
+    TIMESTAMP '2018-10-08 16:00:00',    -- Replace between quotes with actual date and time (YYYY-MM-DD hh:mm:ss).
+                                        -- Delete entire line above and type NULL (without quotes) if it's 0 or 1 above
+    'Vote!',                 -- or name of website, if "location" is online
+    'https://brittakiwit.typeform.com/to/tgzxoE',                  -- Either google maps url, or url to website. Please include "https://" in URL
+
+    /* Do not use quotes in the following lines */
+    FALSE,  -- TRUE if this is a wiki activity, else FALSE
+    FALSE,  -- TRUE if this is a slack activity, else FALSE
+    FALSE,  -- TRUE if this is a calendar activity, else FALSE
+    TRUE,   -- TRUE if this activity, is related to neither wiki, slack, nor calendar, else FALSE
+
+    TRUE,  -- TRUE if applicable to students enrolled in SE, else FALSE
+    TRUE,  -- TRUE if applicable to students enrolled in ID, else FALSE
+    TRUE,  -- TRUE if applicable to students enrolled in PM, else FALSE
+    FALSE,  -- TRUE if applicable to students who have not yet decided, else FALSE
+
+    FALSE, -- TRUE if task is mandatory, FALSE if task is optional
+
+    FALSE, -- TRUE if this is an outdoor activity
+    FALSE, -- TRUE if this is a formal event/activity
+    FALSE, -- TRUE if it is recommended to bring a swimming suit
+    FALSE, -- TRUE if this is a pet friendly activity / event
+    TRUE, -- TRUE if this is a techie activity or event
+
+    0,  -- 0 if event offers no vegan or vegetarian options. 1 for vegetarian. 2 for vegan.
+    0); -- 0 no weather, 1 cloudy, 2 lightning, 3 rain, 4 snow, 5 sun, 6 windy. Please only choose one option.
+
+
+
